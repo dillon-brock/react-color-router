f688ce0f272620c3f80cfd350c2f8ed9bd0b50d3
Dillon Brock <mdillon1297@gmail.com>
2022-09-26 15:48:27 -0700

    added styling to landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,23 @@
+import './Home.css';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
+  const colors = [];
+  for (let i = 0; i < 12; i++) {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    colors.push(color);
+  }
   return (
-    <Link to='/255/255/255'>
-      <button>Go To Color Router</button>
-    </Link>
+    <div className='container'>
+      <main className='home-main'>
+        {colors.map((color, i) => <div key={i} style={{ backgroundColor: `rgb(${colors[i][0]}, ${colors[i][1]}, ${colors[i][2]})`, width: `100%`, height: `100%` }}></div>)}
+      </main>
+      <Link to='/255/255/255'>
+        <button>Go To Color Router</button>
+      </Link>
+    </div>
   );
 }
