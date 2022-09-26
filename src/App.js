f6fa024787dcede6cde96a6bf1e435b99e9fef32
Dillon Brock <mdillon1297@gmail.com>
2022-09-26 15:06:30 -0700

    added landing page with link to /255/255/255

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Switch>
-        <Route path='/:r/:g/:b' component={Main} />
+        <Route exact path='/' component={Home}/>
+        <Route path='/:r/:g/:b'>
+          <Header />
+          <Main />
+        </Route>
       </Switch>
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <Link to='/255/255/255'>
+      <button>Go To Color Router</button>
+    </Link>
+  );
+}
