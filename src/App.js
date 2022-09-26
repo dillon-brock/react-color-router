551cdf395c09b28ffc30932b95a8ee14ae7ab4d4
Dillon Brock <mdillon1297@gmail.com>
2022-09-26 15:09:01 -0700

    added NotFound component, displays with invalid url parameters

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Header />
           <Main />
         </Route>
+        <Route path='*' component={NotFound} />
       </Switch>
     </div>
   );
